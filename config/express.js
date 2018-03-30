fd1415721a958fd2640b79f207e37f5c2216c41a
lauridsen <pjuker@gmail.com>
2018-03-31 00:28:16 +0200

    Changed resave to false to prevent race conditions

--- config/express.js
+++ config/express.js
@@ -1,26 +1,26 @@
-const config = require("./config");
-const express = require("express");
-const morgan = require("morgan");
-const compress = require("compression");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const flash = require("connect-flash");
-const passport = require("passport");
-const session = require("express-session");
+const config = require('./config');
+const express = require('express');
+const morgan = require('morgan');
+const compress = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const passport = require('passport');
+const session = require('express-session');
 
-module.exports = function () {
+module.exports = function() {
   const app = express();
 
-  if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
-  } else if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
     app.use(compress());
   }
 
   app.use(
     bodyParser.urlencoded({
-      extended: true
-    })
+      extended: true,
+    }),
   );
   app.use(bodyParser.json());
   app.use(methodOverride());
@@ -28,22 +28,22 @@ module.exports = function () {
   app.use(
     session({
       saveUninitialized: true,
-      resave: true,
-      secret: config.sessionSecret
-    })
+      resave: false,
+      secret: config.sessionSecret,
+    }),
   );
 
-  app.set("views", "./app/views");
-  app.set("view engine", "ejs");
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
 
   app.use(flash());
   app.use(passport.initialize());
   app.use(passport.session());
 
-  require("../app/routes/index.server.routes.js")(app);
-  require("../app/routes/users.server.routes.js")(app);
-  require("../app/routes/blog.server.routes.js")(app);
+  require('../app/routes/index.server.routes.js')(app);
+  require('../app/routes/users.server.routes.js')(app);
+  require('../app/routes/blog.server.routes.js')(app);
 
-  app.use(express.static("./public"));
+  app.use(express.static('./public'));
   return app;
 };
